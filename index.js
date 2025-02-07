aa1604c1892bc8711992abeb97c1724513df71da
Ulug'bek <rahmonovulugbek20042004@gmail.com>
2025-02-07 17:18:19 +0500

    views

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const morgan = require("morgan");
 // const helmet = require('helmet')
 // const morgan = require('morgan')
 // app.use(morgan('tiny'))   -> yordamida log yozish amalga oshiriladi yani dasturimiga kelgan barcha http surovlarini  log qiladi 
-// app.use(helmet())  ->  https responsga xavsizlikga oid bulgan hederlarni quyib berib dasturimizni xavfsizligin taminlab beradi 
+// app.use(helmet())  ->  https responsga xavsizlikga oid bulgan hederlarni quyib berib dasturimizni xavfsizligini taminlab beradi 
 app.use(express.json());   ///->  request bodysini json formatga o'girib beradi   va http://localhost:5000/readme.txt   shu ko'rinishda faylni yozamiz brouzerga
 app.use(express.urlencoded({extended: true}));  /// -> serverga kelgan so'rov bodysini urlencoded formatini pars qilish uchun ishlatiladi  Asosan frontenda formda bolib ma'lumot kitishini taminlaydi   {extended: true} -> bu kodni yozmasak bizga ogohlantirish keladi extended xosasini quygin deb
 app.use(express.static('public')) /// -> static kontentni ham hoisting qilishimiz mumkin 
@@ -19,7 +19,7 @@ app.use(express.static('public')) /// -> static kontentni ham hoisting qilishimi
 //   app.use(morgan('tiny'));
 //   console.log('Logir ishlayapti...');
 //   }   ///-> bu bizga dasturimiz qaysi organda ishlayotganini qilishimi mumkin
-// console.log(process.env.NODE_EVN); /// ->  organ chiqadaydi Sababi biz organ olganim yoq'   olish uchun  terminlaga    //set  NODE_EVN=production  va tap bosib  //echo  % NODE_EVN% tapni bosak   Bizni asturimiz production   utadi 
+// console.log(process.env.NODE_EVN); /// ->  organ chiqadaydi Sababi biz organ olganim yoq'   olish uchun  terminlaga    //set  NODE_EVN=production  va tap bosib  //echo  % NODE_EVN% tapni bosak   Bizni dasturimiz production   utadi 
 // console.log(app.get('env')); /// -> development organ chiqadi sababi  aftomatiski oladi get('env')
 
 
