18e288fa10fa04dfbad61f0446b8ff5df6e5451d
Ulug'bek <rahmonovulugbek20042004@gmail.com>
2025-02-11 20:06:35 +0500

    routelarga bulish

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 router.get("/", (req, res) => {
     res.send("Hello Node js");
-    // res.render('index', {title: 'my express app',  greeting:"Assalomu alaykum"} )
+    // res.render('index', {title: 'my express app',  greeting:"Assalomu alaykum va rahmatulloh"} )
   });
   module.exports = router
